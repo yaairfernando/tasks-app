4708159f35e6c925a7c5ad3200d73b029d950724
Yair fernando <yair.facio11@gmail.com>
2020-01-07 10:15:09 -0600

    Setting up initial components

---
+++ app/javascript/components/TodoCreate.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class TodoCreate extends Component {
+  render() {
+    return(
+      <div>
+        TodoCreate
+      </div>
+    )
+  };
+};
+
+export default TodoCreate;
---
+++ app/javascript/components/TodoListItem.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class TodoListItem extends Component {
+  render() {
+    return(
+      <div>
+        TodoListItem
+      </div>
+    )
+  };
+};
+
+export default TodoListItem;
